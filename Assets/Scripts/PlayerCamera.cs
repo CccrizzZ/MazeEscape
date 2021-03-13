1e3c86e16662fb5257da12161e14a23275084575
cccrizzz <ccccrizzzz@gmail.com>
2021-03-13 16:43:24 +0800

    Added third person controller

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class PlayerCamera : MonoBehaviour
+{
+  
+    public float MouseSensitivity;
+    
+    float XRotation = 0.0f;
+    float YRotation = 0.0f;
+
+    public bool canLook;
+
+    public Transform CamTransform;
+    PlayerMovement pmove;
+
+    void Start()
+    {
+        pmove = GetComponent<PlayerMovement>();
+        ResetCameraRot();
+        SetCameraMove();
+    }
+
+
+    public void ResetCameraRot()
+    {
+        transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        XRotation = 0.0f;
+    }
+
+
+    public void SetCameraMove()
+    {
+        // lock cursor to the center of screen
+        canLook = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void SetCameraFreeze()
+    {
+        // dont lock cursor
+        canLook = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+    }
+
+
+    void Update()
+    {
+    }
+
+    
+
+
+
+
+    void OnLook(InputValue input)
+    {
+        
+        if (canLook)
+        {
+
+            // // set mouse input
+            // float MouseX = input.Get<Vector2>().x * MouseSensitivity / 500;
+            // float MouseY = input.Get<Vector2>().y * MouseSensitivity / 500;
+            
+            // XRotation += MouseX;
+            // YRotation += MouseY;
+            // YRotation += Mathf.Clamp(YRotation, -10.0f, 10.0f);
+            
+
+            // // // clamp rotation to +90 and -90 deg
+            // XRotation -= MouseY;
+            // XRotation = Mathf.Clamp(XRotation, -90.0f, 90.0f);
+
+            // print(CamTransform.rotation.eulerAngles);
+
+            // rotate player body
+            // transform.localRotation = Quaternion.Euler(0.0f, XRotation, 0.0f);
+            transform.localRotation = Quaternion.Euler(0.0f,CamTransform.rotation.eulerAngles.y, 0.0f);
+
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,162 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float WalkSpeed;
+    public float RunSpeed;
+    public float JumpForce;    
+    public bool isRunning;
+    public bool isJumping;
+    public bool isGrounded;
+    public bool canMove;
+    public bool isPaused;
+
+    Rigidbody rbRef;
+    Animator animController;
+
+    Vector2 InputVector;
+    Vector3 MoveDirection;
+    float CurrentSpeed;
+
+
+
+    void Start()
+    {
+        canMove = true;
+        isGrounded = true;
+        rbRef = GetComponent<Rigidbody>();
+        animController = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(canMove)
+        {
+
+            // determine player direction
+            MoveDirection = transform.forward * InputVector.y + transform.right * InputVector.x;
+
+            // apply velocity if in air
+            if(!isGrounded)
+            {
+                rbRef.velocity += MoveDirection / 10;
+            }
+    
+    
+            // if is jumping dont move
+            if(isJumping) return;
+
+            // if no input dont move
+            if(InputVector.magnitude <= 0) return;
+
+
+            // determine walking or running 
+            if (isRunning)
+            {
+                CurrentSpeed = RunSpeed;
+            }
+            else
+            {
+                CurrentSpeed = WalkSpeed;
+            }
+
+
+
+
+            // make movement vector
+            Vector3 movement = MoveDirection * (CurrentSpeed * Time.deltaTime);
+
+            // apply movement
+            transform.position += movement;
+
+
+            // rbRef.MovePosition(transform.position + movement * 5);
+
+        }
+    }
+    
+    public void OnMove(InputValue value)
+    {
+        if (Time.timeScale == 1)
+        {
+            
+            // get input vector from input value
+            InputVector = value.Get<Vector2>();
+
+            if (InputVector.y > 0)
+            {
+                animController.SetFloat("YMovement", InputVector.y * 0.25f);
+            }
+            else
+            {
+                animController.SetFloat("YMovement", InputVector.y * 0.5f);
+            }
+
+            animController.SetFloat("XMovement", InputVector.x * 0.5f);
+
+
+            // print(MoveDirection);
+        }
+
+
+    }
+
+
+    public void OnRun(InputValue input)
+    {
+        if (input.Get().ToString()=="1")
+        {
+            isRunning = true;
+            if(InputVector.y>0)
+            {
+                animController.SetFloat("YMovement", 0.5f);
+            }
+        }
+        else
+        {
+            isRunning = false;
+            if(InputVector.y > 0)
+            {
+                animController.SetFloat("YMovement", 0.25f);
+            }
+            else
+            {
+                animController.SetFloat("YMovement", 0.0f);
+                
+            }
+        }
+    }
+
+
+    public void OnJump(InputValue input)
+    {
+        if(!isJumping && isGrounded && Time.timeScale == 1)
+        {
+            // // reset velocity
+            // rbRef.velocity = Vector3.zero;
+
+            // set jump bool
+            isJumping = input.isPressed;
+
+            // add force to rigidbody
+            rbRef.AddForce((transform.up + MoveDirection ) * (JumpForce + CurrentSpeed) / 2 , ForceMode.Impulse);
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            print("Grounded");
+            isGrounded = true;
+            isJumping = false;
+        }
+    }
+}
